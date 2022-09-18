e0704b630a1a2205bbc6d1ff82f21b18ddec1ca0
Владимир Матвеев <110328797+VladimirMatveev98@users.noreply.github.com>
2022-09-18 11:18:00 +0300

    Update task 1319.py

--- task 1319.py
+++ task 1319.py
@@ -2,18 +2,23 @@ import re
 
 """Переставить и вывести на экран слова заданного предложения
 в соответствии с ростом доли согласных в этих словах"""
-
+#in progress
 consonat = ['б', 'в', 'г', 'д', 'ж', 'з', 'й', 'к', 'л', 'м', 'н', 'п',
             'р', 'с', 'т', 'ф', 'х', 'ц', 'ч', 'ш', 'щ']
 
+
+words = []
+keys = []
 test_message = "Таким образом, постоянное обеспечение нашей деятельности..."
 ratio = {}
 c = 0
+i = 0
 priority = float(0)
 res = str()
 
 #message = str(input("Введите предложение: "))
 message = test_message
+print(message)
 message = message.lower()
 message = re.split(r'[ ,-.!?:;"]', message)
 
@@ -23,15 +28,17 @@ for word in message:
             if letter in consonat:
                 c = c + 1
         priority = c / len(word)
-        ratio[priority] = word
+        #print (priority)
+        ratio[i] = priority
+        words.append(word)
         priority = 0
         c = 0
+        i += 1
 
-keys = list(ratio.keys())
-keys.sort()
+print(ratio)
+print(words)
 
-for key in keys:
-    word = ratio[key]
-    res = res + word + ' '
+for key in ratio:
+    print ("%s -> %s" % (key, ratio[key]))
 
-print (res)
+#print (res)
